2274c731bd0b63f1abd32c11d459d25c3f4fdf76
Christine Fletcher <chrismis79@gmail.com>
2020-09-10 15:27:42 -0500

    setting up routes

--- hackernews-clone-app/src/App.js
+++ hackernews-clone-app/src/App.js
@@ -1,14 +1,27 @@
 import React from 'react';
+import { Route, Switch } from "react-router-dom"
 
-import NavBar from './components/NavBar'
+import Articles from './pages/Articles';
+import Home from './pages/Home';
+import Jobs from './pages/Jobs';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hacker News</h1>
-        <NavBar />
+
+        <Switch>
+          <Route path="/articles">
+            <Articles />
+          </Route>
+          <Route path="/jobs">
+            <Jobs />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
 
       </header>
     </div>
---
+++ hackernews-clone-app/src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavBar from '../components/NavBar';
+
+const Home = () => {
+    return (
+        <>
+            <h1>Hacker News</h1>
+            <NavBar />
+        </>
+    );
+}
+
+export default Home;
